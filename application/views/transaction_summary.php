4b172b442061d27a1fa3523862e3aa78ad32696d
jfox015 <jfox015@gmail.com>
2020-05-21 23:58:23 -0400

    Width Fix

--- application/views/transaction_summary.php
+++ application/views/transaction_summary.php
@@ -1,4 +1,4 @@
-				<table class="sortable-table" cellpadding="3" cellspacing="1" width="96%" style="border:1px solid #606060;">
+				<table class="sortable-table" cellpadding="3" cellspacing="1" width="100%" style="border:1px solid #606060;">
 				<?php if (!isset($showEffective) || (isset($showEffective) && $showEffective != -1)) { $cols = 4; } else { $cols = 3; } ?>
                 <thead>
                 <tr class='title'>
